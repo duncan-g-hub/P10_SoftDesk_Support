4f1b1fad0fe1b4125424f16580203dc8f26115d6
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-04-14 14:54:59 +0200

    manage UserViewSet with overloading mixins

--- SoftDesk/accounts/views.py
+++ SoftDesk/accounts/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-
+from rest_framework import mixins, viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from accounts.models import User
 from accounts.serializers import UserSerializer
 
-
-
-class UserViewset(viewsets.ModelViewSet):
+class UserViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
     serializer_class = UserSerializer
 
     def get_queryset(self):
-        return User.objects.all()
+        return User.objects.filter(id=self.request.user.id)
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def perform_create(self, serializer):
+        serializer.save()
