cd9d420ec58e904e734a92f356fad36c4d1cf184
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-04-14 14:52:20 +0200

    serializers : create and update user

--- SoftDesk/accounts/serializers.py
+++ SoftDesk/accounts/serializers.py
@@ -4,7 +4,7 @@ from accounts.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    birth_date = serializers.DateField(format='%d/%m/%Y %H:%M')
+    birth_date = serializers.DateField(format='%d/%m/%Y')
     password = serializers.CharField(write_only=True)
 
     class Meta:
@@ -17,7 +17,15 @@ class UserSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({"birth_date": "Ce champ est obligatoire."})
         return data
 
-    def create_user(self, validated_data):
+    def create(self, validated_data):
         # créer l'utilisateur en hachant le mdp
         user = User.objects.create_user(**validated_data)
         return user
+
+    def update(self, instance, validated_data):
+        # si le password est modifié, on le hache
+        password = validated_data.pop('password', None)
+        if password:
+            instance.set_password(password)
+        # mise à jour des autres champs
+        return super().update(instance, validated_data)
