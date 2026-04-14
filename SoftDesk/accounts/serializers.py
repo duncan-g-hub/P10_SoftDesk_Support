e30fe4dd0459982ce721ed7d17af049b8e3a4377
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-04-14 14:18:38 +0200

    feature : create user

--- SoftDesk/accounts/serializers.py
+++ SoftDesk/accounts/serializers.py
@@ -2,8 +2,11 @@ from rest_framework import serializers
 
 from accounts.models import User
 
+
 class UserSerializer(serializers.ModelSerializer):
-    birth_date = serializers.DateTimeField(format='%d/%m/%Y %H:%M')
+    birth_date = serializers.DateField(format='%d/%m/%Y %H:%M')
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = ('id', 'username', 'password', 'birth_date', 'can_be_contacted', 'can_data_be_shared')
@@ -13,3 +16,8 @@ class UserSerializer(serializers.ModelSerializer):
         if not data.get('birth_date'):
             raise serializers.ValidationError({"birth_date": "Ce champ est obligatoire."})
         return data
+
+    def create_user(self, validated_data):
+        # créer l'utilisateur en hachant le mdp
+        user = User.objects.create_user(**validated_data)
+        return user
