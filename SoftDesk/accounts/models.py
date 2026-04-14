e4363dea5954d598fb4447826227c074fbce68d4
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-04-14 13:47:53 +0200

    add user fields

--- SoftDesk/accounts/models.py
+++ SoftDesk/accounts/models.py
@@ -5,5 +5,9 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
 
-    pass
+    birth_date = models.DateField(verbose_name='Date de naissance',null=True, blank=True)
+    can_be_contacted = models.BooleanField(default=False)
+    can_data_be_shared = models.BooleanField(default=False)
+
+
 
