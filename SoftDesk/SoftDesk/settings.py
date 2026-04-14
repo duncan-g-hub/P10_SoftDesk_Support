9e5478b0934ecc38e39a5665ec070fa92eab1d7a
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-04-14 11:57:39 +0200

    add custom user model

--- SoftDesk/SoftDesk/settings.py
+++ SoftDesk/SoftDesk/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'accounts',
     'projects',
 ]
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# use custom user model
+AUTH_USER_MODEL = 'accounts.User'
