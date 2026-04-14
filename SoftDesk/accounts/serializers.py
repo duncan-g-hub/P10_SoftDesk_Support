f71fdf288f0cdec016c6e8f6530e0ca13dc1ddda
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-04-14 13:55:43 +0200

    add validation birth_date in UserSerializer

--- SoftDesk/accounts/serializers.py
+++ SoftDesk/accounts/serializers.py
@@ -1,8 +1,15 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from accounts.models import User
 
-class UserSerializer(ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    birth_date = serializers.DateTimeField(format='%d/%m/%Y %H:%M')
     class Meta:
         model = User
         fields = ('id', 'username', 'password', 'birth_date', 'can_be_contacted', 'can_data_be_shared')
+
+    def validate(self, data):
+        # birth_date obligatoire
+        if not data.get('birth_date'):
+            raise serializers.ValidationError({"birth_date": "Ce champ est obligatoire."})
+        return data
