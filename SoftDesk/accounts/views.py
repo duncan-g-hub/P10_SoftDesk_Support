2b3f170ced28da933bf55c7fc257a778bc21883e
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-04-14 14:11:40 +0200

    Add UserViewset

--- SoftDesk/accounts/views.py
+++ SoftDesk/accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+
+from accounts.models import User
+from accounts.serializers import UserSerializer
+
+
+
+class UserViewset(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        return User.objects.all()
