f11ee55469546f35d519e6ffac0c9251173c11b0
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-04-15 11:37:22 +0200

    add models : Project ; Contributor

--- SoftDesk/projects/models.py
+++ SoftDesk/projects/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from SoftDesk.accounts.models import User
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    type = models.CharField(max_length=3, choices=[
+        ("BCK", "back-end"),
+        ("FRT", "front-end"),
+        ("IOS", "iOS"),
+        ("ADR", "Android")
+    ])
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects') # auteur du projet
+    created_time = models.DateTimeField(auto_now_add=True)
+
+
+
+class Contributor(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='contributions') # contributeur d'un projet
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='contributors') # projet contribués
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'project')  # un user ne peut pas être contributeur plusieurs fois
