7eb5acf7393e64d77593e8b9d47879642b1d1661
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-04-17 11:15:01 +0200

    add created_time field

--- SoftDesk/accounts/models.py
+++ SoftDesk/accounts/models.py
@@ -8,6 +8,7 @@ class User(AbstractUser):
     birth_date = models.DateField(verbose_name='Date de naissance',null=True, blank=True)
     can_be_contacted = models.BooleanField(default=False)
     can_data_be_shared = models.BooleanField(default=False)
+    created_time = models.DateTimeField(auto_now_add=True)
 
 
 
